0770a7ba6f5cf2f76faf3955ff4443cc321117fe
mathiassellanes <mathias@neocoast.com>
2024-11-15 17:46:48 -0300

    add validator

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -3,23 +3,28 @@ import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 
 import { login } from "../dataaccess/auth.js";
+import validateSchema from "../middlewares/validator.js";
+import loginSchema from "./validators/auth.js";
 
 dotenv.config();
 
 const router = Router();
 
-router.post('/login', async (req, res) => {
-  const loginUser = await login(req?.body?.mail, req?.body?.password);
+router.post(
+  '/login',
+  validateSchema(loginSchema),
+  async (req, res) => {
+    const loginUser = await login(req?.body?.mail, req?.body?.password);
 
-  if (loginUser === null) {
-    res.status(401).send('Usuario no encontrado');
-    return;
-  }
+    if (loginUser === null) {
+      res.status(401).send('Usuario no encontrado');
+      return;
+    }
 
-  res.json({
-    correo: loginUser.correo,
-    token: jwt.sign({ correo: loginUser.correo }, process.env.SECRET_KEY),
+    res.json({
+      correo: loginUser.correo,
+      token: jwt.sign({ correo: loginUser.correo }, process.env.SECRET_KEY),
+    });
   });
-});
 
 export default router;

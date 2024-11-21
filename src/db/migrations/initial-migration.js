24c8d7460cc0a2fcc302bf88d68e6d73ed96a941
NahuelPajon <nahuel.pajon@correo.ucu.edu.uy>
2024-11-21 19:31:58 -0300

    triggers as validators

--- src/db/migrations/initial-migration.js
+++ src/db/migrations/initial-migration.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-console */
 
-import connection from '../connection.js';
+import connection from "../connection.js";
 
 connection.connect();
 
@@ -105,6 +105,81 @@ const createTablesQuery = `
   LEFT JOIN Clase c ON t.id = c.id_turno AND c.dictada = TRUE
   GROUP BY t.id, t.hora_inicio, t.hora_fin
   ORDER BY total_clases_dictadas DESC;
+
+  -- Trigger para validar que un alumno no pueda registrarse en dos clases al mismo tiempo
+  DELIMITER //
+CREATE TRIGGER validar_horario_clase
+BEFORE INSERT ON Alumno_Clase
+FOR EACH ROW
+BEGIN
+  DECLARE conflicto INT;
+
+  SELECT COUNT(*)
+  INTO conflicto
+  FROM Alumno_Clase AC
+  JOIN Clase C1 ON AC.id_clase = C1.id
+  JOIN Clase C2 ON C2.id = NEW.id_clase
+  WHERE AC.ci_alumno = NEW.ci_alumno
+    AND C1.id_turno = C2.id_turno
+    AND C1.dia_para_dictar = C2.dia_para_dictar;
+
+  IF conflicto > 0 THEN
+    SIGNAL SQLSTATE '45000'
+    SET MESSAGE_TEXT = 'El alumno ya está registrado en otra clase al mismo tiempo.';
+  END IF;
+END //
+DELIMITER ;
+
+
+  -- Trigger para validar que el alumno tenga la edad suficiente para la actividad
+  DELIMITER //
+CREATE TRIGGER validar_edad_alumno
+BEFORE INSERT ON Alumno_Clase
+FOR EACH ROW
+BEGIN
+  DECLARE edad_alumno INT;
+
+  SELECT TIMESTAMPDIFF(YEAR, A.fecha_nacimiento, CURDATE())
+  INTO edad_alumno
+  FROM Alumnos A
+  WHERE A.ci = NEW.ci_alumno;
+
+  DECLARE edad_requerida INT;
+
+  SELECT edad_minima
+  INTO edad_requerida
+  FROM Actividades Act
+  JOIN Clase C ON Act.id = C.id_actividad
+  WHERE C.id = NEW.id_clase;
+
+  IF edad_alumno < edad_requerida THEN
+    SIGNAL SQLSTATE '45000'
+    SET MESSAGE_TEXT = 'El alumno no cumple con la edad mínima requerida para esta actividad.';
+  END IF;
+END //
+DELIMITER ;
+
+  -- Trigger para validar que el alumno use el equipamiento de la clase de la actividad correspondiente
+  DELIMITER //
+CREATE TRIGGER validar_equipamiento_clase
+BEFORE INSERT ON Alumno_Clase
+FOR EACH ROW
+BEGIN
+  DECLARE equipamiento_valido INT;
+
+  SELECT COUNT(*)
+  INTO equipamiento_valido
+  FROM Equipamiento E
+  JOIN Clase C ON E.id_actividad = C.id_actividad
+  WHERE E.id = NEW.id_equipamiento
+    AND C.id = NEW.id_clase;
+
+  IF equipamiento_valido = 0 THEN
+    SIGNAL SQLSTATE '45000'
+    SET MESSAGE_TEXT = 'El equipamiento no corresponde a la actividad de esta clase.';
+  END IF;
+END //
+DELIMITER ;
 `;
 
 const queries = createTablesQuery
